b623d15565fd04c10c68a5989e937eec7e95f858
seacevedo <seacevedo@seacevedo.localdomain>
2023-04-05 10:33:24 -0500

    updated curl command in setup script

--- setup.sh
+++ setup.sh
@@ -22,7 +22,6 @@ cd ..
 
 git clone https://github.com/seacevedo/Solana-Pipeline.git
 cd Solana-Pipeline
-echo '${SERVICE_ACCOUNT_JSON}' >> service_account_keyfile.json
 
 sudo apt-get update -y
 sudo apt install python3-pip -y
@@ -31,6 +30,8 @@ python3 -m venv solana-pipeline-env
 source solana-pipeline-env/bin/activate
 pip3 install -r requirements.txt
 python3 -m spacy download en_core_web_sm
+
+curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/SERVICE_ACCOUNT_JSON" >> service_account_keyfile.json
 
 
 
