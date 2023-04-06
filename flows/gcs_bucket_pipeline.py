2ccd86cda62adb6f6409d89b635dc3cd81c50619
Saul Acevedo <saul.acevedo@sbgenomics.com>
2023-04-06 09:19:39 -0500

    Update gcs_bucket_pipeline.py

--- flows/gcs_bucket_pipeline.py
+++ flows/gcs_bucket_pipeline.py
@@ -10,6 +10,10 @@ from output_manager import *
 
 
 def reddit_client(client_id: str, client_secret: str, reddit_username: str) -> praw.Reddit:
+    
+    '''
+    Set up PRAw instance
+    '''
 
     reddit_api = praw.Reddit(
         client_id=client_id,
@@ -21,12 +25,18 @@ def reddit_client(client_id: str, client_secret: str, reddit_username: str) -> p
     return reddit_api
 
 def submission_fetcher(submission, output_manager: OutputManager):
+    '''
+    Get the submission data 
+    '''
     submission_data_list = (submission.id, submission.author.name, submission.title, 
                             [submission.selftext.replace(',', '')], submission.ups, submission.downs, 
                             datetime.utcfromtimestamp(submission.created_utc), submission.url)                    
     output_manager.submissions_list.append(submission_data_list)
 
 def comments_fetcher(submission, output_manager: OutputManager, reddit_api: praw.Reddit):
+    '''
+    Get the comments for each post (capped at 200 for posts that have more comments
+    '''
     try:
         submission_rich_data = reddit_api.submission(id=submission.id)
         if submission_rich_data.num_comments <= 200:
@@ -51,11 +61,17 @@ def comments_fetcher(submission, output_manager: OutputManager, reddit_api: praw
 
 @task(name="Writing subreddit data to gcs bucket")
 def write_gcs(path: Path) -> None:
+    '''
+    Write data to gcs bucket
+    '''
     gcs_block = GcsBucket.load("crypto-reddit")
     gcs_block.upload_from_folder(from_folder=path, to_folder=path)
 
 
 @task(name="Fetching Subreddit Data", log_prints=True, retries=3)
+'''
+   Gets subreddit and post data that is N days old
+'''
 def fetch_subreddit_data(client_id: str, client_secret: str, reddit_username: str, num_days: int, output_manager: OutputManager, spark: SparkSession):
     
     output_manager.reset_lists()
