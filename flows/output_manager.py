00c665492cef5b948633503080ec18ac8bba89aa
Saul Acevedo <saul.acevedo@sbgenomics.com>
2023-04-08 13:15:41 -0500

    Update output_manager.py

--- flows/output_manager.py
+++ flows/output_manager.py
@@ -1,24 +1,22 @@
-from pyspark.sql import SparkSession
 from datetime import datetime
-from pyspark.sql import types
-from os.path import join
+import pandas as pd
+from os.path import join, exists
+import os
+
 
 class OutputManager:
     # Class helps maintain some paths to write files to local machine and bucket
-    def __init__(self, subreddit: str, subreddit_cap: int, partition_num: int, bucket_dir: str):
+    def __init__(self, subreddit: str, subreddit_cap: int):
         self.submissions_list = []
         self.subreddit = subreddit
         self.subreddit_cap = subreddit_cap
-        self.partition_num = partition_num
-        self.bucket_dir = bucket_dir
         self.comments_list = []
         self.run_id = datetime.now().strftime("%Y%m%d")
 
         self.runtime_dir = join(self.subreddit, self.run_id)
-        self.bq_upload_dir = join(self.bucket_dir, self.run_id)
 
-        self.submissions_output = join(self.runtime_dir, "submissions")
-        self.comments_output = join(self.runtime_dir, "comments")
+        self.submissions_output = join(self.runtime_dir, "submissions.csv")
+        self.comments_output = join(self.runtime_dir, "comments.csv")
 
 
 
@@ -27,34 +25,17 @@ class OutputManager:
         self.submissions_list = []
         self.comments_list = []
 
-    def store(self, spark: SparkSession):
+    def store(self):
         # write post and comment data to local environment
-
-        sub_schema = types.StructType([
-            types.StructField('submission_id', types.StringType(), True),
-            types.StructField('submission_author', types.StringType(), True),
-            types.StructField('submission_title', types.StringType(), True),
-            types.StructField('submission_text', types.StringType(), True),
-            types.StructField('submission_upvotes', types.IntegerType(), True),
-            types.StructField('submission_downvotes', types.IntegerType(), True),
-            types.StructField('submission_created_time', types.TimestampType(), True),
-            types.StructField('submission_url', types.StringType(), True)
-        ])
-
-        com_schema = types.StructType([
-            types.StructField('submission_id', types.StringType(), True),
-            types.StructField('comment_id', types.StringType(), True),
-            types.StructField('comment_author', types.StringType(), True),
-            types.StructField('comment_text', types.StringType(), True),
-            types.StructField('comment_upvotes', types.IntegerType(), True),
-            types.StructField('comment_downvotes', types.IntegerType(), True),
-            types.StructField('comment_created_time', types.TimestampType(), True),
-            types.StructField('comment_url', types.StringType(), True)
-        ])
-
-        df_submission = spark.createDataFrame(data=self.submissions_list,schema=sub_schema)
-        df_submission = df_submission.repartition(self.partition_num)
-        df_submission.write.mode('overwrite').parquet(self.submissions_output)
-        df_comment = spark.createDataFrame(data=self.comments_list,schema=com_schema)
-        df_comment = df_comment.repartition(self.partition_num)
-        df_comment.write.mode('overwrite').parquet(self.comments_output)
+        if exists(self.runtime_dir) == False:
+            os.makedirs(self.runtime_dir)
+        df_submission = pd.DataFrame(data=self.submissions_list,columns=['submission_id', 'submission_author', 
+                                                                         'submission_title', 'submission_text', 
+                                                                         'submission_upvotes', 'submission_downvotes',
+                                                                         'submission_created_time', 'submission_url'])
+        df_submission.to_csv(self.submissions_output, index=False)
+        df_comment = pd.DataFrame(data=self.comments_list,columns=['submission_id', 'comment_id',
+                                                                   'comment_author', 'comment_text', 
+                                                                   'comment_upvotes', 'comment_downvotes',
+                                                                   'comment_created_time', 'comment_url'])
+        df_comment.to_csv(self.comments_output, index=False)
