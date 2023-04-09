7400e953f01f9d79a28a81945b7dfa08164e7d74
Saul Acevedo <saul.acevedo@sbgenomics.com>
2023-04-09 09:58:03 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,5 @@
 #!/bin/bash 
 
-mkdir ~/spark
-cd ~/spark
-wget https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
-tar xzfv openjdk-11.0.2_linux-x64_bin.tar.gz
-echo 'export JAVA_HOME="${HOME}/spark/jdk-11.0.2"' >> ~/.bashrc 
-echo 'export PATH="${JAVA_HOME}/bin:${PATH}"' >> ~/.bashrc 
-rm openjdk-11.0.2_linux-x64_bin.tar.gz
-
-
-wget https://dlcdn.apache.org/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3.tgz
-tar xzfv spark-3.3.2-bin-hadoop3.tgz
-rm spark-3.3.2-bin-hadoop3.tgz
-echo 'export SPARK_HOME="${HOME}/spark/spark-3.3.2-bin-hadoop3"' >> ~/.bashrc 
-echo 'export PATH="${SPARK_HOME}/bin:${PATH}"' >> ~/.bashrc 
-
-echo 'export PYTHONPATH="${SPARK_HOME}/python/:$PYTHONPATH"' >> ~/.bashrc 
-echo 'export PYTHONPATH="${SPARK_HOME}/python/lib/py4j-0.10.9.5-src.zip:$PYTHONPATH"' >> ~/.bashrc 
-
-cd ..
-
 git clone https://github.com/seacevedo/Solana-Pipeline.git
 cd Solana-Pipeline
 
